144cc9bfb292a9b67566e965f8cc71aee4f0d8a4
Erin Skidds <dudethatserin@outlook.com>
2021-09-25 15:18:10 -0400

    Updated to V13... help.js does not work but everything else does.

--- commands/misc/ping.js
+++ commands/misc/ping.js
@@ -2,10 +2,8 @@ const { Client, CommandInteraction } = require('discord.js');
 
 module.exports = {
   name: 'ping',
-  aliases: ['pong', 'beep', 'boop'],
   categories: 'Miscellaneous',
   description: 'Check to make sure the bot is online.',
-  usage: '/ping',
   /** 
    * @param {Client} client 
    * @param {CommandInteraction} interaction 
